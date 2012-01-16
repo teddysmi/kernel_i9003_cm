ddc407a93bf55e984c65aaec1a326fdffaead270
vurrut <calvoredondo@gmail.com>
2012-01-16 11:55:31 +0100

    change structure

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 0 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 PREEMPT Sun Jan 15 18:44:41 CET 2012"
+#define LINUX_COMPILE_TIME "18:44:41"
+#define LINUX_COMPILE_BY "vurrut"
+#define LINUX_COMPILE_HOST "cm"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
